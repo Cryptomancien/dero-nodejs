a7534c54280fa73bf26f2d036bf6e73d3154738c
netwarp <resethread@gmail.com>
2020-06-10 13:10:08 +0200

    add some methods

--- src/Wallet.js
+++ src/Wallet.js
@@ -1,11 +1,15 @@
 const http = require('http');
 
 class Wallet {
-
-	constructor(address = '127.0.0.1', port = '20209', network = 'mainnet') {
+	/**
+	 *
+	 * @param {String} address
+	 * @param {String} port
+	 * Remember: port 20209 for mainnet 30306 for testnet
+	 */
+	constructor(address = '127.0.0.1', port = '20209') {
 		this.address = address;
 		this.port = port;
-		this.network = network;
 	}
 
 	async request(method, params, verb = 'POST', path = '/json_rpc') {
@@ -23,7 +27,7 @@ class Wallet {
 				// console.log(data)
 			}
 
-			data = JSON.stringify(data)
+			data = JSON.stringify(data);
 
 			const options = {
 				hostname: this.address,
@@ -34,7 +38,7 @@ class Wallet {
 					'Content-Type': 'application/json',
 					'Content-Length': data.length
 				}
-			}
+			};
 
 			let rawData = '';
 
@@ -61,14 +65,32 @@ class Wallet {
 		})
 	}
 
+	/**
+	 *
+	 * @returns {Promise<unknown>}
+	 */
 	getaddress() {
 		return this.request('getaddress').then(data => {
 			return data
 		})
 	}
 
-	getbalance() {
+	/**
+	 * @param {Boolean} format
+	 * @returns {Promise<unknown>}
+	 */
+	getbalance(format = false) {
 		return this.request('getbalance').then(data => {
+
+
+			let balance = data.result.balance.toLocaleString();
+			let unlocked_balance = data.result.unlocked_balance.toLocaleString();
+
+			data.formatNumber = {
+				balance,
+				unlocked_balance
+			};
+
 			return data
 		})
 	}
@@ -84,13 +106,32 @@ class Wallet {
 
 	/**
 	 * 
-	 *	option = {
-	 *		destination
-	 *	}
-	 *
+	 *	@param {Number} amount
+	 *	@param {String} address
+	 *	@param {Number} mixin
+	 *	@param {String} payment_id
 	 */
-	transfer(options) {
-		return this.request('transfer', options).then(data => {
+	transfer(amount, address, Mixin = 6, payment_id = '') {
+
+		const params = {
+			destinations :[
+				{
+					amount: amount ,
+					address: address
+				}
+			],
+			Mixin: 6,
+			"unlock_time":0,
+			"payment_id": payment_id,
+			"get_tx_key": true,
+			"priority":1.0,
+			"do_not_relay": false,
+			"get_tx_hex": true
+		}
+
+		//return new Promise(resolve => resolve(options))
+
+		return this.request('transfer', params).then(data => {
 			return data
 		})
 	}
@@ -103,26 +144,72 @@ class Wallet {
 
 	}
 
+	/**
+	 *
+	 * @returns {Promise<unknown>}
+	 */
 	query_key() {
-
+		return this.request('query_key', {key_type: 'view_key '}).then(data => {
+			return data
+		})
 	}
 
 	make_integrated_address() {
 
 	}
 
-	split_integrated_address() {
+	/**
+	 *
+	 * @param {String} integrated_address
+	 * @returns {Promise<unknown>}
+	 */
+	split_integrated_address(integrated_address) {
 
+		const params = {
+			integrated_address
+		}
+
+		return this.request('split_integrated_address', params).then(data => {
+			return data
+		})
 	}
 
-	get_transfer_by_txid() {
+	/**
+	 *
+	 * @param {String} txid
+	 * @returns {Promise<unknown>}
+	 */
+	get_transfer_by_txid(txid) {
 
+		const params = {
+			txid
+		}
+
+		return this.request('get_transfer_by_txid', params).then(data => {
+			return data
+		})
 	}
 
-	get_transfers() {
+	/**
+	 *
+	 * @param {Boolean} In
+	 * @param {Boolean} Out
+	 * @param {Number} min_height
+	 * @param {Number} max_height
+	 */
+	get_transfers(In = true, Out = true, min_height = 0, max_height = 0) {
 
+		const params = {
+			In,
+			Out,
+			min_height,
+			max_height
+		}
+
+		return this.request('get_transfers', params).then(data => {
+			return data
+		})
 	}
-
 }
 
 module.exports = Wallet;
